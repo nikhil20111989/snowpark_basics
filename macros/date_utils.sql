3441c96825f88615a9ff36d9a67900b85a545619
Nikhil Kulkarni <nikhil_kulkarni6@persistent.com>
2026-02-02 11:18:15 +0000

    daily weather model created

---
+++ macros/date_utils.sql
@@ -0,0 +1,29 @@
+{% macro function1(x) %}
+
+CASE when to_timestamp({{x}}) < current_date then 'PAST'
+else 'FUTURE' END 
+
+{% endmacro %}
+
+{% macro get_season(x) %}
+
+case when month(to_timestamp({{x}})) in (12,1,2)
+then 'WINTER'
+when month(to_timestamp({{x}})) in (3,4,5)
+then 'SPRING'
+when month(to_timestamp({{x}})) in (6,7,8)
+then 'SUMMER'
+else 'AUTOMN'
+end 
+
+{% endmacro %}
+
+
+
+{% macro day_type(x) %}
+case
+when dayname(to_timestamp({{x}})) in ('Sat','Sun')
+then 'WEEKEND'
+else 'BUSINESS-DAY' 
+end 
+{% endmacro %}
--- models/example/date_dimension.sql
+++ models/example/date_dimension.sql
@@ -3,8 +3,8 @@ SELECT
 to_timestamp(STARTED_AT) as STARTED_AT,
 date(to_timestamp(STARTED_AT)) as Date_started_at,
 hour(to_timestamp(STARTED_AT)) as hour_started_at,
-dayofweek(to_timestamp(STARTED_AT)) as week_day,
-dayname(to_timestamp(STARTED_AT)) as week_day_name,
+--dayofweek(to_timestamp(STARTED_AT)) as week_day,
+--dayname(to_timestamp(STARTED_AT)) as week_day_name,
 
 CASE
 WHEN dayname(to_timestamp(STARTED_AT)) in ('Sat','Sun')
@@ -12,7 +12,11 @@ THEN 'WEEKEND'
 ELSE 'BUSINESS-DAY' 
 END as day_type,
 
-monthname(to_timestamp(STARTED_AT)) as month 
+{{day_type('STARTED_AT')}},
+
+{{get_season('STARTED_AT')}},
+
+{{function1('STARTED_AT')}}
 
 FROM {{ source('demo', 'bike') }}
 where STARTED_AT != 'started_at'
