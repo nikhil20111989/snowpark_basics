3da6458dac8e5df2ed1419a5548c439a9b6f25b1
Nikhil Kulkarni <nikhil_kulkarni6@persistent.com>
2026-02-02 11:34:48 +0000

    station dimension created

---
+++ models/example/station_dimension.sql
@@ -0,0 +1,12 @@
+with bike as(
+    select 
+    start_station_id,
+    start_station_name,
+    start_lat,
+    start_lng
+    from {{ source('demo', 'bike') }}
+    where ride_id !='ride_id'
+ 
+)
+
+select * from bike
