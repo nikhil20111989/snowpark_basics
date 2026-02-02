6444607b9dceb8dca3568c0c47d30e48dd678260
Nikhil Kulkarni <nikhil_kulkarni6@persistent.com>
2026-02-02 10:09:22 +0000

    date_dimension model

---
+++ models/example/date_dimension.sql
@@ -0,0 +1,20 @@
+WITH CTE AS (
+SELECT 
+to_timestamp(STARTED_AT) as STARTED_AT,
+date(to_timestamp(STARTED_AT)) as Date_started_at,
+hour(to_timestamp(STARTED_AT)) as hour_started_at,
+dayofweek(to_timestamp(STARTED_AT)) as week_day,
+dayname(to_timestamp(STARTED_AT)) as week_day_name,
+
+CASE
+WHEN dayname(to_timestamp(STARTED_AT)) in ('Sat','Sun')
+THEN 'WEEKEND'
+ELSE 'BUSINESS-DAY' 
+END as day_type,
+
+monthname(to_timestamp(STARTED_AT)) as month 
+
+FROM {{ source('demo', 'bike') }}
+where STARTED_AT != 'started_at'
+)
+SELECT * FROM CTE
